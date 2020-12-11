a18cb7630db78833e43b36f1c146370de71a5c1e
Shaun van Ardenne <s.vanardenne@gmail.com>
2020-12-11 11:26:25 -0800

    Changed config info

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Form from './Form'
 import Nav from './Nav';
 import PhotoContainer from './PhotoContainer';
-import apiKey from '../config'
+import apiKey from '../config';
 
 function App() {
   return (

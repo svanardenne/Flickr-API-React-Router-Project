ac3c8069acac57cf23722b246856de1132604bb9
Shaun van Ardenne <s.vanardenne@gmail.com>
2020-12-16 15:18:07 -0800

    Removed the x from the cat, dog, and computers categories

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,8 +8,14 @@ const Nav = (props) => {
       <ul>
         {props.links.map((link, index) => (
           <li className="nav-link" key={index}>
-          <NavLink to={`/${link}`}>{`${link.charAt(0).toUpperCase() + link.slice(1)}`}</NavLink>
-          <button className="remove-button" onClick={() => props.removeSearchLink(link)}>X</button></li>
+            <NavLink to={`/${link}`}>{`${link.charAt(0).toUpperCase() + link.slice(1)}`}</NavLink>
+              {index > 2
+              ?
+              <button className="remove-button" onClick={() => props.removeSearchLink(link)}>X</button>
+              :
+              null
+              }
+          </li>
         ))}
       </ul>
 
